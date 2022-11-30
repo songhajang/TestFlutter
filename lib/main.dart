c95cd4ab770daa07d688f70723694c96c3887b45
songhajang <angpan2004@gmail.com>
2022-11-30 17:03:45 +0900

    flutter router 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '#/': (context) => JSONGet(), // 초기 라우트로 FirstScreen을 설정
+        '/second': (context) =>
+            SecondScreen(), // '/second'란 경로로 SecondScrrent을 설정정
+      },
       home: Scaffold(
         body: Center(
           child: JSONGet(),
@@ -65,10 +71,34 @@ class _JSONGetState extends State<JSONGet> {
             child: Padding(
               padding: EdgeInsets.all(16.0),
               child: new TextButton(
-                  onPressed: null, child: Text(data[index]["title"])),
+                  onPressed: () {
+                    // 라우트의 이름으로 새로운 라우트 적재
+                    Navigator.pushNamed(context, '/second');
+                  },
+                  child: Text(data[index]["title"])),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            // 현재 라우트를 pop(제거)
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
       ),
     );
   }
